783c903a9fff2e6655910d6965d1c126b7a3b37b
Jamie Collinson <jamiecollinson@gmail.com>
2016-09-29 10:32:20 +0100

    Add functions for 1.2.1

--- 1.2.el
+++ 1.2.el
@@ -3,3 +3,23 @@
 ;; 1.2 Procedures and the Processes They Generate
 
 ;; 1.2.1 Linear Recursion and Iteration
+
+(defun factorial (n)
+  (if (= n 1)
+      1
+    (* n (factorial (- n 1)))))
+
+(factorial 6)
+720
+
+(defun factorial (n)
+  (defun fact-iter (product counter max-count)
+    (if (> counter max-count)
+        product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+  (fact-iter 1 1 n))
+
+(factorial 6)
+720
