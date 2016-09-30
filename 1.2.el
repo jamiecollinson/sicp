7d41e532ef8999f878cce6c739eaafad313bac4d
Jamie Collinson <jamiecollinson@gmail.com>
2016-09-30 17:05:22 +0100

    Add fib example

--- 1.2.el
+++ 1.2.el
@@ -85,3 +85,24 @@
 ;; => h(n) = ?
 
 ;; 1.2.2 Tree Recursion
+
+(defun fib (n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (t (+ (fib (- n 1))
+              (fib (- n 2))))))
+
+(fib 5)
+5
+
+(defun fib (n)
+  (defun fib-iter (a b count)
+    (if (= count 0)
+        b
+      (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
+
+(fib 5)
+5
+
+;; Example: Counting change
