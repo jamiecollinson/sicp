94f9caff012e0aa99ffda9d277b8445aa2bf9cbd
Jamie Collinson <jamiecollinson@gmail.com>
2016-10-02 08:36:11 +0100

    Add skeleton of 1.19

--- 1.2.el
+++ 1.2.el
@@ -284,3 +284,21 @@
 4
 (*-fast 10 100)
 1000
+
+;; Exercise 1.19
+
+(defun fib (n)
+  (defun fib-iter (a b p q count)
+    (cond ((= count 0) b)
+          ((even? count)
+           (fib-iter a
+                     b
+                     ?
+                     ?
+                     (/ count 2)))
+          (t (fib-iter (+ (* b q) (* a q) (* a p))
+                       (+ (* b p) (* a q))
+                       p
+                       q
+                       (- count 1)))))
+  (fib-iter 1 0 0 1 n))
