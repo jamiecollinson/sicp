3e7ef0481dedd1830e7a086f6d823c143d0d07d2
Jamie Collinson <jamiecollinson@gmail.com>
2016-12-16 17:11:47 +0000

    Add 1.3.1

--- 1.3.el
+++ 1.3.el
@@ -7,3 +7,33 @@
 
 (sum-integers 1 3)
 6
+
+(defun sum-cubes (a b)
+  (defun cube (a) (* a a a))
+  (if (> a b)
+      0
+    (+ (cube a) (sum-cubes (+ a 1) b))))
+
+(sum-cubes 1 3)
+36
+
+(defun pi-sum (a b)
+  (if (> a b)
+      0
+    (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+(pi-sum 1 3)
+0.3333333333333333
+
+(defun sum (term a next b)
+  (if (> a b)
+      0
+    (+ (term a)
+       (sum term (next a) next b))))
+
+(defun inc (n) (+ n 1))
+(defun cube (n) (* n n n))
+(defun sum-cubes (a b)
+  (sum cube a inc b))
+
+(sum-cubes 1 3)
